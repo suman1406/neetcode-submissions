380af29fa027658dcf03197e40def181ed924a5b
Suman Panigrahi <119001618+suman1406@users.noreply.github.com>
2026-05-28 09:34:14 +0530

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows = len(heights)
+        cols = len(heights[0])
+
+        pacific = set()
+        atlantic = set()
+
+        def dfs(r, c, visited, prev_height):
+            if (
+                r < 0 or r >= rows or
+                c < 0 or c >= cols or
+                (r, c) in visited or
+                heights[r][c] < prev_height
+            ):
+                return
+
+            visited.add((r, c))
+
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
+
+        for c in range(cols):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(rows - 1, c, atlantic, heights[rows -1][c])
+
+        for r in range(rows):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, cols - 1, atlantic, heights[r][cols - 1])
+
+        res = []
+
+        for r in range(rows):
+            for c in range(cols):
+                if (r, c) in pacific and (r, c) in atlantic:
+                    res.append([r, c])
+
+        return res
